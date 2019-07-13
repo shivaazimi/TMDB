ec26d6c199f8b5ec0bfa0b9015d017d379aacff5
shivaazimi <shivaazimi7625@gmail.com>
2019-07-13 14:59:33 +0430

    third commit

--- app/src/main/java/com/example/imdb/Adapter/RecyclerAdapter.java
+++ app/src/main/java/com/example/imdb/Adapter/RecyclerAdapter.java
@@ -11,6 +11,7 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.example.imdb.Model.Model;
 import com.example.imdb.Model.Movie;
 import com.example.imdb.R;
 import com.squareup.picasso.Picasso;
@@ -19,23 +20,23 @@ import java.util.ArrayList;
 
 public class GridRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    public int viewTypeItem=0;
-    public int viewTypeLoading=1;
+    public int viewTypeItem = 0;
+    public int viewTypeLoading = 1;
     public Context context;
-    public ArrayList<Movie> movies;
+    public ArrayList<Model> list;
 
-    public GridRecyclerAdapter(Context context , ArrayList<Movie> movies){
-        this.context=context;
-        this.movies=movies;
+    public GridRecyclerAdapter(Context context, ArrayList<Model> list) {
+        this.context = context;
+        this.list = list;
     }
 
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
-        if(viewType==viewTypeItem){
-            View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.grid_recycle_item,parent,false);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (viewType == viewTypeItem) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.grid_recycle_item, parent, false);
             return new MovieViewHolder(view);
-        }else if(viewType==viewTypeLoading) {
-            View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.progress_bar_item,parent,false);
+        } else if (viewType == viewTypeLoading) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.progress_bar_item, parent, false);
             return new LoadingViewHolder(view);
         }
         return null;
@@ -43,26 +44,26 @@ public class GridRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewH
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        if(holder instanceof MovieViewHolder){
-            Movie movie=movies.get(position);
-            MovieViewHolder movieViewHolder=(MovieViewHolder) holder;
+        if (holder instanceof MovieViewHolder) {
+            Movie movie = (Movie) list.get(position);
+            MovieViewHolder movieViewHolder = (MovieViewHolder) holder;
             Picasso.with(context).load(movie.getPoster()).fit().into(movieViewHolder.poster);
             movieViewHolder.title.setText(movie.getTitle());
             movieViewHolder.description.setText(movie.getPlot());
-        }else if(holder instanceof LoadingViewHolder){
-            LoadingViewHolder loadingViewHolder=(LoadingViewHolder) holder;
+        } else if (holder instanceof LoadingViewHolder) {
+            LoadingViewHolder loadingViewHolder = (LoadingViewHolder) holder;
             loadingViewHolder.progressBar.setIndeterminate(true);
         }
     }
 
     @Override
     public int getItemCount() {
-        return movies==null ? 0 : movies.size();
+        return list == null ? 0 : list.size();
     }
 
     @Override
     public int getItemViewType(int position) {
-        return movies.get(position)==null ? viewTypeLoading :viewTypeItem;
+        return list.get(position) == null ? viewTypeLoading : viewTypeItem;
     }
 
     static class MovieViewHolder extends RecyclerView.ViewHolder {
---
+++ app/src/main/java/com/example/imdb/Adapter/ViewPagerAdapter.java
@@ -0,0 +1,4 @@
+package com.example.imdb.Adapter;
+
+public class FragmentPagerAdapter {
+}
